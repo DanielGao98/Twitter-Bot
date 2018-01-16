02f5af9ab03d50b470f095bd1ccaab4d4600ba07
Daniel Gao <dannygao0818@gmail.com>
2018-01-16 15:45:41 -0800

    Add files via upload
    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 't7V7fNCJI6DRYDqMXyWmcQFwb',  
+  consumer_secret: 'AQNhLWDb2Bzi393AoTMS5V6oYtsojmqXkKKDu6WuVdtdPyh9Kp',
+  access_token: '842455657684992000-5nPXGJ30mtkORy6SPdde4LeVzUJw3qK',  
+  access_token_secret: 'P0h8M6Uq82omMZPI6byHZiijy7gF1oFnob7unjJsxlHNX'
+}
